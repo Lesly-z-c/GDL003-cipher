555d04908a27d66f1e63879e9384977b3a88d4f2
unknown <leslalazc@gmail.com>
2019-06-07 19:56:07 -0500

    decifrado con estilos

--- src/index.js
+++ src/index.js
@@ -1,25 +1,52 @@
 const texto = document.getElementById("mensaje");
+const desplazamiento =document.getElementById("saltos")
 
 function comenzar (){
-let palabra =  [];
+ let palabra =  [];
  let textoUsuario = texto.value.toUpperCase();
  let mensajeCesar = "";
  console.log(textoUsuario);
-
+ const valorDesplazamiento = parseInt(desplazamiento.value);
  //document.getElementById("mensaje").value;
- for(var i = 0; i < textoUsuario.length; i++) {
+  for(var i = 0; i < textoUsuario.length; i++) {
    console.log(textoUsuario[i])
    let mensajeEnAscii = textoUsuario[i].charCodeAt(0);
    console.log(mensajeEnAscii);
-    let transformacion= (mensajeEnAscii -65+4)%26+65;
+    let transformacion= (mensajeEnAscii -65+ valorDesplazamiento)%26+65;
     mensajeCesar += String.fromCharCode(transformacion);
     mensaje+=mensajeCesar;
     }
     console.log(mensajeCesar);
     document.getElementById("mensajeCesar").innerHTML = "Tu texto en codigo es :" + mensajeCesar;
-
     return mensajeCesar;
 }
 // Funciones con parametros function(string, offset)
 document.getElementById("boton1").addEventListener("click", comenzar);
 //addEventListener('click', function() {
+//aqui empieza el desifrado
+const nuevoTexto = document.getElementById("mismomensaje");
+const mismoDesplazamiento = document.getElementById("mismoSalto");
+
+function regresar (){
+  //let mismoTexto = [];
+  let regresamensaje = nuevoTexto.value;
+  let mensajeSinCesar = '';
+  console.log(regresamensaje);
+  const nuevoDesplazamiento = parseInt(mismoDesplazamiento.value);
+
+  for(var i = 0; i < regresamensaje.length; i++) {
+   console.log(regresamensaje[i]);
+   let mensajeEnAscii1 = regresamensaje[i].charCodeAt(0);
+   console.log(mensajeEnAscii1);
+    let transformacion2 = (mensajeEnAscii1 - 65 - nuevoDesplazamiento)%26+65;
+    mensajeSinCesar += String.fromCharCode(transformacion2);
+    //mismomensaje += mensajeSinCesar;
+    }
+ return mensajeSinCesar;
+}
+
+
+document.getElementById("boton2").addEventListener("click", function() {
+  let mensajeDescifrado = regresar();
+  document.getElementById("mensajeSinCesar").innerHTML = "tu texto original es :" + mensajeDescifrado;
+});
