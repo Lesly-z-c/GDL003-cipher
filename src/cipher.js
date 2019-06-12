8c02637989352aeea8faa64c509bfecab7bebf83
unknown <leslalazc@gmail.com>
2019-06-12 14:30:40 -0500

    se egrega decode funcional en test

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,41 @@
 window.cipher = {
-  // ... 
+  // ...
+
+ encode : (otrodesplazamiento, textoUsuario) => {
+
+   let mensajeCesar = "";
+
+
+
+    for(var i = 0; i < textoUsuario.length; i++) {
+     console.log(textoUsuario[i]);
+     let mensajeEnAscii = textoUsuario[i].charCodeAt(0);
+     console.log(mensajeEnAscii);
+      let transformacion= (mensajeEnAscii -65 + otrodesplazamiento)%26+65;
+      mensajeCesar += String.fromCharCode(transformacion);
+
+
+      console.log(mensajeCesar);
+}
+
+      return mensajeCesar;
+},
+
+ decode : (mismoDesplazamiento, nuevoTexto) => {
+    //let mismoTexto = [];
+    let mensajeSinCesar = '';
+    for(let i = 0; i <nuevoTexto.length; i++) {
+      console.log(+nuevoTexto[i]);
+      let mensajeEnAscii1 = nuevoTexto.charCodeAt(i);
+      console.log(mensajeEnAscii1);
+      if (mensajeEnAscii1 >= 65 && mensajeEnAscii1 <= 90) {
+         let transformacion2 = (mensajeEnAscii1 + 65 -  mismoDesplazamiento) % 26 + 65;
+          mensajeSinCesar += String.fromCharCode(transformacion2);
+            //mismomensaje += mensajeSinCesar;
+      }
+        console.log(mensajeSinCesar);
+    }
+    console.log(mensajeSinCesar);
+   return mensajeSinCesar;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,58 +1,31 @@
-const texto = document.getElementById("mensaje");
-const desplazamiento =document.getElementById("saltos")
 
-function comenzar (){
- let palabra =  [];
- let textoUsuario = texto.value.toUpperCase();
- let mensajeCesar = "";
- console.log(textoUsuario);
- const valorDesplazamiento = parseInt(desplazamiento.value);
- //document.getElementById("mensaje").value;
-  for(var i = 0; i < textoUsuario.length; i++) {
-   console.log(textoUsuario[i])
-   let mensajeEnAscii = textoUsuario[i].charCodeAt(0);
-   console.log(mensajeEnAscii);
-    let transformacion= (mensajeEnAscii -65+ valorDesplazamiento)%26+65;
-    mensajeCesar += String.fromCharCode(transformacion);
-    mensaje+=mensajeCesar;
-    }
-    console.log(mensajeCesar);
+
+let texto = document.getElementById("mensaje");
+const desplazamiento =document.getElementById("saltos");
+let imprimir = () => {
+  document.getElementById("codif").style.display = "none";
+  document.getElementById("decodif").style.display = "block";
+};
+ let encode = ()=> {
+    let textoUsuario = document.getElementById("mensaje").value;
+    //const valorDesplazamiento = parseInt(desplazamiento.value);
+    let valorDesplazamiento = parseInt(desplazamiento.value);
+    console.log(valorDesplazamiento, texto);
+    let mensajeCesar = window.cipher.encode(valorDesplazamiento, textoUsuario);
     document.getElementById("mensajeCesar").innerHTML = "Tu texto en codigo es :" + mensajeCesar;
-    return mensajeCesar;
-}
-function imprimir() {
-  codif.style.display = "none"
-  decodif.style.display = "block"
 
-}
+ };
 // Funciones con parametros function(string, offset)
-document.getElementById("boton1").addEventListener("click", comenzar);
-document.getElementById("boton1").addEventListener("click", imprimir); //si pongo esta linea no me imprime el mensaje codificado y me salta a decodificar, pero si la quito si lo decodifica, pero no me manda a decodificar
-//addEventListener('click', function() {
-//aqui empieza el desifrado
-const nuevoTexto = document.getElementById("mismomensaje");
-const mismoDesplazamiento = document.getElementById("mismoSalto");
+document.getElementById("boton1").addEventListener("click", encode);
+document.getElementById("decodadecof").addEventListener("click", imprimir); //si pongo esta linea no me imprime el mensaje codificado y me salta a decodificar, pero si la quito si lo decodifica, pero no me manda a decodificar
 
-function regresar (){
-  //let mismoTexto = [];
-  let regresamensaje = nuevoTexto.value;
-  let mensajeSinCesar = '';
-  console.log(regresamensaje);
-  const nuevoDesplazamiento = parseInt(mismoDesplazamiento.value);
+const desplazamiento2 = document.getElementById("mismoSalto");
+ let decode = ()=> {
+   let nuevoTexto = document.getElementById("mismomensaje").value;
+   let mismoDesplazamiento = parseInt(desplazamiento2.value);
+   console.log(mismoDesplazamiento, nuevoTexto);
+   let mensajeSinCesar = window.cipher.decode(mismoDesplazamiento, nuevoTexto);
+  document.getElementById("mensajeSinCesar").innerHTML = "Tu texto original es :" + mensajeSinCesar;
+};
 
-  for(var i = 0; i < regresamensaje.length; i++) {
-   console.log(regresamensaje[i]);
-   let mensajeEnAscii1 = regresamensaje[i].charCodeAt(0);
-   console.log(mensajeEnAscii1);
-    let transformacion2 = (mensajeEnAscii1 - 65 - nuevoDesplazamiento)%26+65;
-    mensajeSinCesar += String.fromCharCode(transformacion2);
-    //mismomensaje += mensajeSinCesar;
-    }
- return mensajeSinCesar;
-}
-
-
-document.getElementById("boton2").addEventListener("click", function() {
-  let mensajeDescifrado = regresar();
-  document.getElementById("mensajeSinCesar").innerHTML = "tu texto original es :" + mensajeDescifrado;
-});
+document.getElementById("boton2").addEventListener("click", decode);
